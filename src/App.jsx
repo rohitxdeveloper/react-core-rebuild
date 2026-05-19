79281336627149c52df319db4c157a131b6d8be4
Rohit Tomar <rohitxdeveloper@gmail.com>
2026-05-19 18:26:00 +0530

    fic: correct component import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Header from "./components/Header";
+
 export default function App() {
   return (
+    <>
+    <Header></Header>
     <div>
       <h1>test workflow update  🚀</h1>
     </div>
+    </>
   );
 }
